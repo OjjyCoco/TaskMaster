6ba184d33f1361e80cb8f7bfe7d23184ddfb91b6
NathanWSL2 <nathanheckmann@gmail.com>
2025-05-15 23:48:37 +0200

    Adding localhost path in comment

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -76,6 +76,8 @@ serve(async (req) => {
     const session = await stripe.checkout.sessions.create({
       success_url: 'https://task-master-blond-five.vercel.app/dashboard',
       cancel_url: 'https://task-master-blond-five.vercel.app/pricing',
+      // success_url: 'http://localhost:8080/dashboard',
+      // cancel_url: 'http://localhost:8080/pricing',
       mode: 'subscription',
       customer: customer.id,
       customer_update: {
